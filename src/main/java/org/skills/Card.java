8ee705111107c06f397671fa442eb97e4180118f
Ralf Wirdemann <ralf.wirdemann@gmail.com>
2018-05-17 21:33:45 +0200

    Make result list more dynamic

---
+++ src/main/java/org/skills/Card.java
@@ -0,0 +1,23 @@
+package org.skills;
+
+public class Card {
+
+    private String description;
+    private String image;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
--- src/main/java/org/skills/SearchController.java
+++ src/main/java/org/skills/SearchController.java
@@ -1,8 +1,12 @@
 package org.skills;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class SearchController {
@@ -13,7 +17,17 @@ public class SearchController {
     }
 
     @GetMapping("/result")
-    public String result() {
+    public String result(Model model) {
+        Card c1 = new Card();
+        c1.setDescription("Ich programmiere seit 18 Jahren in Java und habe alles gesehen. Ich kann sehr gut lehren und programmiere gerne selber.");
+        c1.setImage("ralf");
+
+        Card c2 = new Card();
+        c2.setDescription("Ich programmiere seit 18 Jahren in Java und habe alles gesehen. Ich kann sehr gut lehren und programmiere gerne selber.");
+        c2.setImage("placeholder");
+
+        List<Card> cards = Arrays.asList(c1, c2);
+        model.addAttribute("cards", cards);
         return "result";
     }
 
